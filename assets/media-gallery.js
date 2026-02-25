047fadc9f4f1d28e270836a878bc6bb10de0eae5
Dejan Mitrovic <dejanmitrovic1910@proton.me>
2026-02-25 16:16:18 +0100

    chore: live store sync

--- assets/media-gallery.js
+++ assets/media-gallery.js
@@ -56,7 +56,6 @@ if (!customElements.get('media-gallery')) {
           if (!this.mql.matches || this.elements.thumbnails) {
             activeMedia.parentElement.scrollTo({ left: activeMedia.offsetLeft });
           }
-
           if (prepend) return;
           const activeMediaRect = activeMedia.getBoundingClientRect();
           // Don't scroll if the image is already in view
